a369823e9f47dfb53ee4306574c3e9818001cdf1
VictoriaNaenfeldtEklund <victoria.naenfeldt.eklund@iths.se>
2024-12-04 14:26:32 +0100

    Cleaned up unused imports in LoginController

--- src/main/java/com/example/fitquest/Controller/LoginController.java
+++ src/main/java/com/example/fitquest/Controller/LoginController.java
@@ -3,15 +3,9 @@ package com.example.fitquest.Controller;
 import com.example.fitquest.Model.LoginModel;
 import com.example.fitquest.Screen.NewScene;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
-
-import java.io.IOException;
-
 
 public class LoginController {
 

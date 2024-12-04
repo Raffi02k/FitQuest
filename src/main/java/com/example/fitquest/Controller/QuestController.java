03fd30e6ef62901786be2572b0d397c6e7ce2423
VictoriaNaenfeldtEklund <victoria.naenfeldt.eklund@iths.se>
2024-12-04 14:39:08 +0100

    Switched to new Sceneloader in QuestController.

--- src/main/java/com/example/fitquest/Controller/QuestController.java
+++ src/main/java/com/example/fitquest/Controller/QuestController.java
@@ -1,31 +1,10 @@
 package com.example.fitquest.Controller;
 
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
-
-import java.io.IOException;
+import com.example.fitquest.Screen.NewScene;
 
 public class QuestController {
-    @FXML
-    private AnchorPane QuestGridPane;
 
-    @FXML
-    public void onMenuClick() {
-        loadNewScene("/com/example/fitquest/Menu-view.fxml");
-
-    }
-
-    private void loadNewScene(String fxmlPath) {
-        try {
-            Stage stage = (Stage) QuestGridPane.getScene().getWindow(); // Hämta nuvarande fönster
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(fxmlPath));
-            Scene scene = new Scene(fxmlLoader.load(), 400, 800);
-            stage.setScene(scene);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public void onMenuClick(){
+        NewScene.getInstance().loadNewScene("/com/example/fitquest/Menu-view.fxml");
     }
 }

45f39f586d56de757f569adcdc6e4e8bb8ba5af8
VictoriaNaenfeldtEklund <victoria.naenfeldt.eklund@iths.se>
2024-12-04 14:35:27 +0100

    Cleaned up code from old sceneloader in MyQuestsController

--- src/main/java/com/example/fitquest/Controller/MyQuestsController.java
+++ src/main/java/com/example/fitquest/Controller/MyQuestsController.java
@@ -3,15 +3,7 @@ package com.example.fitquest.Controller;
 import com.example.fitquest.Model.MyQuestsModel;
 import com.example.fitquest.Screen.NewScene;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class MyQuestsController {
 
@@ -22,26 +14,6 @@ public class MyQuestsController {
     private TextArea myQuestsDescriptionTextArea;
     @FXML
     private Label myQuestsUserScore;
-
-    @FXML
-    private VBox MyQuestGridPane;
-
-    @FXML
-    public void onMenuClick(){
-        NewScene.getInstance().loadNewScene("/com/example/fitquest/Menu-view.fxml");
-//        loadNewScene("/com/example/fitquest/Menu-view.fxml");
-    }
-
-    private void loadNewScene(String fxmlPath) {
-        try {
-            Stage stage = (Stage) MyQuestGridPane.getScene().getWindow(); // Hämta nuvarande fönster
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(fxmlPath));
-            Scene scene = new Scene(fxmlLoader.load(), 400, 800);
-            stage.setScene(scene);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
     /**
      * Initierar instans till myQuestsModel.
@@ -70,5 +42,9 @@ public class MyQuestsController {
     public void myQuestsFinishButtonClicked() {
         int selectedIndex = myQuestsList.getSelectionModel().getSelectedIndex();
         myQuestsModel.processFinishedQuest(selectedIndex);
+    }
+
+    public void onMenuClick(){
+        NewScene.getInstance().loadNewScene("/com/example/fitquest/Menu-view.fxml");
     }
 }
